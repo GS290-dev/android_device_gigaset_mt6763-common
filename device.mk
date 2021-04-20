2b9e8f6adf8ccc3e95eb75c14032fd1628e6e8d2
Kalpaj Chaudhari <daedroza@gmail.com>
2021-04-20 20:26:31 +0530

    GS290: Build android.hardware.bluetooth.audio@2.0-impl
    Change-Id: I1686e6b96a3dcd76938c1b921a89c0843f7f4ded

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
