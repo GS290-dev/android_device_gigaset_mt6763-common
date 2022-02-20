3e32a97d724095c3eedf2febd1eb279bda0d18c7
nift4 <nift4@protonmail.com>
2022-02-20 17:23:00 +0100

    mt6763-common: ims: Apply vtservice shim to libmtk_vt_service as well
    Fixes:
    02-15 18:50:21.231   789   789 F linker  : CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system/lib/libmtk_vt_service.so"...

--- extract-files.sh
+++ extract-files.sh
@@ -39,6 +39,9 @@ function blob_fixup() {
         vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod)
             "${PATCHELF}" --add-needed "libshim_beanpod.so" "${2}"
             ;;
+        lib/libmtk_vt_service.so)
+            "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
+            ;;
     esac
 }
 
